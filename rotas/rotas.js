561bf551c5c107d508efe4ccd91b9230783bb88a
rossiniroberto52 <81196657+rossiniroberto52@users.noreply.github.com>
2021-09-01 15:29:28 -0300

    servidor de envio para memes

--- rotas/rotas.js
+++ rotas/rotas.js
@@ -1,25 +1,24 @@
-const express = require('express')
+const express = require('express');
 const rotas = express.Router()
 
-let urls = [
-    {'tipo':'login', 'info':'loginusr'},
-    {'tipo':'registro', 'info':'register'},
-    {'tipo':'inicial', 'info':'pginicial!'}
-]
+let urlInfo=[
+    {'tipo':'login','info':'login'},
+    {'tipo':'register','info':'registrar'},
+    {'tipo':'acessar','info':'Curso de typescript'}
+];
 
-rotas.get('/', (req,res) => {
-    res.json({ola:'seja bem vindo!'})
+rotas.get('/', (req, res)=>{
+    res.json({ola:'seja bem vindo'})
 });
 
-rotas.get('/:tipo', (req,res) => {
-    const tipos = req.params.tipo;
-    const tiposI = urls.find(i=>i.tipo == tipo);
-    if(!cursoI){
+rotas.get('/:tipoid', (req, res)=>{
+    const tipo = req.params.cursoid;
+    const tipoI = urlInfo.find(i=>i.tipo == tipo);
+    if(!tipoI){
         res.status(404).json(
-            {error:'404 not found!', tipoP:tipo}
+            {error:'tipo encontrado',tipoPesquisado:tipo}
         )
-    }
-    else{
+    }else{
         res.status(200).json(tipoI)
     }
 });
